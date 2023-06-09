95dda1714a8a15a3f5f929fcaf67012ee5911163
Krinesh <pkrinesh95@gmail.com>
2023-06-09 17:37:01 +0530

    feat: home page migration

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,81 @@
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Pressable,
+  FlatList,
+} from 'react-native';
+import { Link, useNavigation } from 'expo-router';
+import { AppBar, Scaffold } from '../src/components/base';
+import { ASSETS } from '../src/common/constant';
+import { useTrips } from '../src/zustand/trip-store';
+import { EmptyState } from '../src/components/empty-state';
 
 export default function Index() {
+  const navigation = useNavigation();
+  const trips = useTrips();
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-lg font-bold">This is home route</Text>
-      <Link href={'add-trip'}> Add Trip </Link>
-      <Link href={'trip-expense'}> My Trips </Link>
-    </View>
+    <Scaffold>
+      <AppBar title="Tripsy" showBackButton={false} titleStyle="text-2xl" />
+      <View className="bg-brandLight justify-center items-center">
+        <Image
+          resizeMode="cover"
+          className="w-screen h-[260px]"
+          source={ASSETS.IMAGES.tripsyBanner2}
+        />
+      </View>
+      <View className="flex-row justify-between items-center my-3 px-4">
+        <Text className="font-[700] text-lg text-black">RECENT TRIPS</Text>
+        <Link href="add-trip" asChild>
+          <Pressable>
+            <View className="bg-black py-3 px-6 rounded-full">
+              <Text className="text-sm font-bold text-brandLight">
+                Add New Trip
+              </Text>
+            </View>
+          </Pressable>
+        </Link>
+      </View>
+      <View className="flex-1 overflow-hidden">
+        <FlatList
+          className="self-center w-screen ml-7"
+          data={trips}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          columnWrapperStyle={{ justifyContent: 'flex-start', marginTop: 10 }}
+          contentContainerStyle={{ paddingBottom: 10 }}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={
+            <EmptyState comment="Looks like you haven't had any trips yet!" />
+          }
+          renderItem={({ item, index }) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => navigation.navigate('trip-expense', { item })}
+              >
+                <View className="bg-white mx-[6] p-2 rounded-2xl">
+                  <Image
+                    source={item.banner}
+                    className="h-[130] w-[150]"
+                    resizeMode="cover"
+                  />
+                  <View style={{ marginLeft: 10, marginTop: 5 }}>
+                    <Text className="text-black font-semibold">
+                      {item.place}
+                    </Text>
+                    <Text className="text-grey text-xs font-semibold">
+                      {item.country}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    </Scaffold>
   );
 }
--- app/trip-expense.tsx
+++ app/trip-expense.tsx
@@ -1,7 +1,12 @@
 import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, useNavigation, useRouter } from 'expo-router';
 
 export default function TripExpense() {
+  const navigation = useNavigation();
+  const route = useRouter();
+
+  console.log(JSON.stringify(navigation.getState().routes, null, 2));
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-lg font-bold">Trip Expense List</Text>
